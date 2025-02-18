913991cc4f498ef245d6e4c44410f1b9cce97a66
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-18 20:40:49 +0900

    [Update]

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    public Transform Player;
+    float offsetX;
+    float offsetY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Player == null)
+        { return; }
+
+        offsetX = transform.position.x - Player.position.x;
+        offsetY = transform.position.y - Player.position.y;
     }
 
     // Update is called once per frame
