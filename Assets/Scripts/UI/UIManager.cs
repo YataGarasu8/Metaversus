6ebc885750ae10917fed8c06a4a738051dcb3555
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-19 19:46:03 +0900

    [Update]

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void OnClickStart()
+    { }
+    public void OnClickExit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;//유니티 플레이 종료
+        Application.Quit();//빌드 된 게임 종료
+    }
+    public void OnClickReturn()
+    { }
+}
