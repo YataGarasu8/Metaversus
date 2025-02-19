949fbad6b58d7c3f133af087e53e3013ef8196b6
YataGarasu8 <zmffldjdnld12@naver.com>
2025-02-19 20:34:01 +0900

    [Update]

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : BaseController
 {
+    public GameObject Fkey;
     protected override void ActionController()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -17,6 +18,20 @@ public class PlayerController : BaseController
         else if (horizontal < 0)
         {
             spriteRenderer.flipX = false; // 오른쪽 이동 시 원래 방향
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)//Collider에서 IsTrigger가 체크가 안되어 있을 때 사용, 매개변수 이름도 차이 남
+    {
+        if (collision.gameObject.CompareTag("Player")) // 충돌한 객체가 플레이어인지 확인
+        {
+            talkBox.SetActive(true);//플레이어가 범위에 있으면 대화 상자 출력 
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            talkBox.SetActive(false); // 플레이어가 벗어나면 자동으로 대화 상자 숨기기
         }
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+
     public static UIManager instance;
     private void Awake()
     {
@@ -13,12 +15,12 @@ public class UIManager : MonoBehaviour
         }
     }
     public void OnClickStart()
-    { }
+    { SceneManager.LoadScene("GameScene"); }
     public void OnClickExit()
     {
         UnityEditor.EditorApplication.isPlaying = false;//유니티 플레이 종료
         Application.Quit();//빌드 된 게임 종료
     }
     public void OnClickReturn()
-    { }
+    { SceneManager.LoadScene("MainScene"); }
 }
